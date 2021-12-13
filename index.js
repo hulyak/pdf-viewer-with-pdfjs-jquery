b48040ee9584e854add4fd69bb5971588916d651
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-12-13 12:34:30 -0800

    working

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const url = 'Document.pdf';
+const url = 'document.pdf';
 
 let $pdfDoc = null,
   $pageNum = 1,
   $pageIsRendering = false,
   $pageNumIsPending = null;
 
-const $scale = 1.5; // size of the pdf
-const $ctx = $('#pdf-render')[0].getContext('2d');
+const $scale = 1.5;
+
+$('#zoom').on('input', function () {
+  // this #zoom
+  $('#zoomValue').text($('#zoom').val() + '%');
+  $pdfDoc.scale = parseInt($('#zoom').val()) / 100;
+  queueRenderPage($pdfDoc);
+});
 
 // Render the page
 const renderPage = (num) => {
   $pageIsRendering = true;
 
-  // Get the page pdfDoc object
-  $pdfDoc.getPage(num).done((page) => {
+  $pdfDoc.getPage(num).then((page) => {
     console.log('page', page);
+    const $ctx = $('#canvas')[0].getContext('2d');
     const $viewport = page.getViewport({ scale: $scale });
-    $('#pdf-render').height = $viewport.height;
-    $('#pdf-render').width = $viewport.width;
+    $('#canvas')[0].height = $viewport.height;
+    $('#canvas')[0].width = $viewport.width;
 
     const renderCtx = {
       canvasContext: $ctx,
@@ -33,7 +39,6 @@ const renderPage = (num) => {
       }
     });
 
-    // Output current page
     $('#page-num').html(num);
   });
 };
@@ -47,25 +52,21 @@ const queueRenderPage = (num) => {
   }
 };
 
-// Get Document
-pdfjsLib
-  .getDocument(url)
-  .promise.then((pdfDoc_) => {
-    $pdfDoc = pdfDoc_;
-    console.log('pdfDocument', $pdfDoc);
-    $('#page-count').html($pdfDoc.numPages);
+// 1. Get Document
+function getDocument() {
+  pdfjsLib
+    .getDocument(url)
+    .promise.then((pdfDoc_) => {
+      $pdfDoc = pdfDoc_;
+      console.log('pdfDocument', $pdfDoc);
+      $('#page-count').html($pdfDoc.numPages);
 
-    renderPage($pageNum);
-  })
-  .fail((err) => {
-    const $div = $('<div></div>')
-      .addClass('error')
-      .append(document.createTextNode(err.message));
-
-    $('body').insertBefore($div, $('#pdf-render'));
-
-    $('.top-bar').css({ display: 'none' });
-  });
+      renderPage($pageNum);
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
 
 const showPrevPage = () => {
   if ($pageNum <= 1) {
@@ -86,3 +87,15 @@ const showNextPage = () => {
 // Button Events
 $('#prev-page').click(showPrevPage);
 $('#next-page').click(showNextPage);
+
+// open a new PDF file
+$('#fileUpload').on('change', function (e) {
+  const $file = e.target.files[0];
+  const $reader = new FileReader();
+  $reader.readAsDataURL($file);
+  $reader.onload = function () {
+    getDocument($reader.result);
+  };
+});
+
+getDocument();
